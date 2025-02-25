f23d35705eece80f41188a745c5f181998432383
ANAURELY DIAS BITENCOURT <anaurely.dias@gmail.com>
2025-02-25 11:16:45 +0000

    //atualizado 25-02-2025

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,11 +6,50 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
+struct Carta {
+    char Codigo[4];
+    char Estado;
+    char Cidade[50];
+    int Populacao;
+    float Area;
+    float PIB;
+    int Turisticos;
+};
+
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    struct Carta carta1;
+
+    printf("Digite o código da carta (ex: A01): ");
+    scanf("%s", &carta1.Codigo);
+
+    printf("Digite a 1ª letra estado (ex: X): ");
+    scanf(" %c", &carta1.Estado);
+
+    printf("Digite a cidade: (ex: CampoGrande) ");
+    scanf("%s", &carta1.Cidade);
+
+    printf("Digite a população(ex: 1(milhões)): ");
+    scanf("%d", &carta1.Populacao);
+
+    printf("Digite a área (ex: 1 km²): ");
+    scanf("%f", &carta1.Area);
+
+    printf("Digite o PIB (ex: 1 bilhões): ");
+    scanf("%f", &carta1.PIB);
+
+    printf("Digite o número de pontos turísticos(ex: 6): ");
+    scanf("%d", &carta1.Turisticos);
+
+    printf("Carta 1 \n");
+    printf("Código da Carta: %s\n", carta1.Codigo);
+    printf("Inicial do Estado: %c\n", carta1.Estado);
+    printf("Nome da Cidade: %s\n", carta1.Cidade);
+    printf("População: %d mil habitantes\n", carta1.Populacao);
+    printf("Área: %.2f km²\n", carta1.Area);
+    printf("PIB: %f bilhões de reais\n", carta1.PIB);
+    printf("Número de pontos turísticos: %d\n", carta1.Turisticos);
+
     
-    // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
@@ -20,3 +59,4 @@ int main() {
 
     return 0;
 }
+
